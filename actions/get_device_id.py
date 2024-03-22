5fcc5bed0498db1dbd45b7f2557fc2b1a8ef54bc
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-03-22 13:11:12 -0700

    removed uneeded refs

--- actions/get_device_id.py
+++ actions/get_device_id.py
@@ -9,7 +9,7 @@ class GetGluDeviceId(action.BaseAction):
             method="GET",
             url=f"{self.glu_base_url}/api/devices",
             params={
-                "orgId": "b131860f-ed86-4a87-a896-3a140e2c4ea9",
+                "orgId": self.org_id,
                 "name": parameters["device_name"],
             },
             headers=None,
