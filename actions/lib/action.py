d7dc6b4bd4260d3e806547affb169af465c3db4f
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-04-07 01:09:45 -0400

    added docs & refactored

--- actions/lib/action.py
+++ actions/lib/action.py
@@ -1,3 +1,27 @@
+"""Defines a base action class for accessing Gluware APIs with authentication credentials and base URL.
+
+This module provides a class `BaseAction` which inherits from `st2common.runners.base_action.Action`. 
+It serves as a base action for accessing Gluware APIs with authentication credentials and base URL.
+
+Example:
+    from st2common.runners.base_action import Action
+
+    class BaseAction(Action):
+        def __init__(self, config):
+            super(BaseAction, self).__init__(config)
+            self.glu_auth = (self.config["glu_username"], self.config["glu_password"])
+            self.glu_base_url = self.config["glu_base_url"]
+            self.org_id = self.config["glu_org_id"]
+
+Attributes:
+    None
+
+Methods:
+    __init__(config): Initializes the base action class with Gluware authentication credentials and base URL.
+
+"""
+
+
 from st2common.runners.base_action import Action
 
 
--- actions/run_workflow.py
+++ actions/run_workflow.py
@@ -1,7 +1,43 @@
+"""Defines an action to run a Gluware workflow with provided parameters.
+
+This module provides a class `RunGluWorklflow` which inherits from `action.BaseAction`. 
+It enables users to run a Gluware workflow with provided parameters.
+
+Example:
+    from lib import action
+    from lib.webmethod import WebMethod
+
+    class RunGluWorklflow(action.BaseAction):
+        def run(self, **parameters):
+            self.web_method = WebMethod(verify=False)
+            response_raw = self.web_method.call(
+                method="POST",
+                url=f"{self.glu_base_url}/api/workflows/{parameters["workflow_id"]}/run",
+                json={
+                    "orgId": self.org_id,
+                    "deviceIds": [parameters["device_id"]],
+                    "inputParameters": {},
+                },
+                headers=None,
+                auth=self.glu_auth,
+            )
+            if response_raw is not None:
+                return response_raw.status_code
+            else:
+                return {"status": "unhandled exception"}
+
+Attributes:
+    None
+
+Methods:
+    run(**parameters): Executes the action to run a Gluware workflow with provided parameters.
+
+"""
+
+
+
 from lib import action
 from lib.webmethod import WebMethod
-
-workflow_id = "af06b8d9-4110-4f45-8149-a53950764913"  #! NTP WF
 
 
 class RunGluWorklflow(action.BaseAction):
@@ -9,7 +45,7 @@ class RunGluWorklflow(action.BaseAction):
         self.web_method = WebMethod(verify=False)
         response_raw = self.web_method.call(
             method="POST",
-            url=f"{self.glu_base_url}/api/workflows/{workflow_id}/run",
+            url=f"{self.glu_base_url}/api/workflows/{parameters["workflow_id"]}/run",
             json={
                 "orgId": self.org_id,
                 "deviceIds": [parameters["device_id"]],
