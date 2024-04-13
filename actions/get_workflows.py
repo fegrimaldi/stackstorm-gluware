693867051738f9a6473bce9179dd6d8cd019163d
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-04-12 20:54:18 -0400

    corrected version

--- actions/get_workflows.py
+++ actions/get_workflows.py
@@ -3,29 +3,30 @@ from lib import action
 from lib.webmethod import WebMethod
 
 
-class GetWorkflows(action.BaseAction):
+class GetGluWorkflows(action.BaseAction):
     def run(self, **parameters):
         self.web_method = WebMethod(verify=False)
 
         response = self.web_method.call(
             method="GET",
-            url=f"{self.glu_base_url}/api/devices",
+            url=f"{self.glu_base_url}/api/workflows",
             params={
                 "orgId": self.org_id,
-                "name": parameters["device_name"],
+                "workflowType": "PRODUCTION",
+                "private": False
             },
             json=None,
             headers=None,
             auth=self.glu_auth,
         )
         if response is not None:
-            devices = response.json()
+            workflows = response.json()
             self.logger.info(
-                "GetGluDeviceId", extra={parameters["device_name"]: devices[0]["id"]}
+                "GetGluWorkflows", extra={}
             )
-            return devices[0]["id"]
+            return workflows
         else:
             self.logger.error(
-                "GetGluDeviceId", extra={"msg": "WebMethod's call response was `None`"}
+                "GetGluWorkflows", extra={"msg": "WebMethod's call response was `None`"}
             )
             sys.exit(1)
