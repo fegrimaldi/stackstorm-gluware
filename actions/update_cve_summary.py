055c083ee71e6bb5cb79d6bab63af5722d5b363f
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-05-11 00:10:22 -0400

    added json dumps

--- actions/update_cve_summary.py
+++ actions/update_cve_summary.py
@@ -18,6 +18,7 @@ Author:
 """
 
 import sys
+import json
 from lib import action
 from lib.webmethod import WebMethod
 
@@ -67,7 +68,7 @@ class UpdateCveSummary(action.BaseAction):
                 response = self.web_method.call(
                     method="PUT",
                     url=f"{self.glu_base_url}/api/devices/{deviceId}",
-                    json=cve_payload,
+                    json=json.dumps(cve_payload),
                     headers=headers,
                     auth=self.glu_auth,
                 )
