f22be4420572f7afa65abbf3524e9d9e9c710c2e
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-05-12 21:25:51 -0400

    added action

---
+++ actions/format.slack_blocks.py
@@ -0,0 +1,13 @@
+from lib import action
+import json
+
+
+class GetBgpState(action.BaseAction):
+    def run(self, **parameters):
+        self.blocks = parameters["block"]
+
+        for key in self.blocks.keys():
+            blocks = self.blocks[key]
+            blockDump = json.dumps(blocks)
+
+        return json.dumps(blockDump)
