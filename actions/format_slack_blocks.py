76a43ffb1daf4ae2b7c157d3ecf9bd488037494a
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-05-13 00:41:08 -0400

    testing

--- actions/format_slack_blocks.py
+++ actions/format_slack_blocks.py
@@ -5,7 +5,9 @@ class FormatSlackBlocks(action.BaseAction):
     def run(self, **parameters):
         self.blocks = json.loads(parameters["blocks"])
 
-        # Extracting values regardless of the key
+        formatted_blocks = []
         for key in self.blocks.keys():
             blocks = self.blocks[key]
-        return blocks
+            formatted_blocks.append(blocks)
+
+        return formatted_blocks
