2d861f8b59f3c8284d931263aef8a7a696a4a06c
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-05-13 00:29:02 -0400

    update

--- actions/format_slack_blocks.py
+++ actions/format_slack_blocks.py
@@ -6,6 +6,6 @@ class FormatSlackBlocks(action.BaseAction):
         self.blocks = parameters["blocks"]
 
         # Extracting values regardless of the key
-        formatted_blocks = list(self.blocks.values())
-
-        return formatted_blocks
+        for key in self.blocks:
+            blocks = self.blocks[key]
+        return blocks
