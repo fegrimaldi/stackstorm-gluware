e2154ddc4606969defae3d16b310f2484672fb86
Fabricio E. Grimaldi <fgrimaldi@gluware.com>
2024-05-13 11:28:33 -0400

    testing

--- actions/format_slack_blocks.py
+++ actions/format_slack_blocks.py
@@ -4,8 +4,10 @@ import json
 class FormatSlackBlocks(action.BaseAction):
     def run(self, **parameters):
         self.blocks = json.loads(parameters["blocks"])
-
+        formatted_blocks = []
         for key in self.blocks.keys():
             blocks = self.blocks[key]
-            return blocks
+            for block in blocks:
+                formatted_blocks.append(block)
+            return formatted_blocks
 
