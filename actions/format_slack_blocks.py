07e13735f2865f7f4d2bf47fa0897038145f3286
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-05-14 14:42:41 -0400

    fixed bug

--- actions/format_slack_blocks.py
+++ actions/format_slack_blocks.py
@@ -4,10 +4,7 @@ import json
 class FormatSlackBlocks(action.BaseAction):
     def run(self, **parameters):
         self.blocks = json.loads(parameters["blocks"])
-        formatted_blocks = []
+
         for key in self.blocks.keys():
             blocks = self.blocks[key]
-            for block in blocks:
-                formatted_blocks.append(block)
-            return formatted_blocks
-
+        return json.dumps(blocks)
