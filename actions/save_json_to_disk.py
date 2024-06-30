f77ed660a3c360ffef6653a93f8d9f5c94e1f945
Fabricio E. Grimaldi <fgrimaldi@gluware.com>
2024-06-30 00:35:59 -0400

    Updated

--- actions/save_json_to_disk.py
+++ actions/save_json_to_disk.py
@@ -2,12 +2,12 @@ import json
 from st2common.runners.base_action import Action
 
 class SaveJsonToDisk(Action):
-    def run(self, payload, file_path):
+    def run(self, **parameters):
         try:
-            with open(file_path, 'w') as f:
-                json.dump(payload, f, indent=4)
-            self.logger.info(f"Payload saved to {file_path}")
-            return True, f"Payload saved to {file_path}"
+            with open(parameters["file_path"], 'w') as f:
+                json.dump(parameters["payload"], f, indent=4)
+            self.logger.info(f"Payload saved to {parameters["file_path"]}")
+            return True, f"Payload saved to {parameters["file_path"]}"
         except Exception as e:
             self.logger.error(f"Failed to save payload: {str(e)}")
             return False, f"Failed to save payload: {str(e)}"
